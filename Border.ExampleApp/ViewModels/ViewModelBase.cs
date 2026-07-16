9aa823243492adf1e702db7b12dc078f622a3298
Jasson_Chou <ss5014871@gmail.com>
2026-07-16 23:40:42 +0800

    Add Border.ExampleApp with Avalonia UI framework

---
+++ Border.ExampleApp/ViewModels/ViewModelBase.cs
@@ -0,0 +1,8 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace Border.ExampleApp.ViewModels
+{
+    public abstract class ViewModelBase : ObservableObject
+    {
+    }
+}
---
+++ Border.ExampleApp/Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace Border.ExampleApp.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
