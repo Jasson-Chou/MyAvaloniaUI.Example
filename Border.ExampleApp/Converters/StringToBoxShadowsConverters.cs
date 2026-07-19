6feba91eeb2497fab1ae7dfd843e87cbaf14a4cd
Jasson_Chou <ss5014871@gmail.com>
2026-07-19 15:50:20 +0800

    Add StringToBoxShadowsConverter for BoxShadow binding

---
+++ Border.ExampleApp/Converters/StringToBoxShadowsConverters.cs
@@ -0,0 +1,40 @@
+﻿using Avalonia.Data;
+using Avalonia.Data.Converters;
+using Avalonia.Media;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Border.ExampleApp.Converters
+{
+    public class StringToBoxShadowsConverters : IValueConverter
+    {
+        public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            if (value is not string text ||
+            string.IsNullOrWhiteSpace(text))
+            {
+                return default(BoxShadows);
+            }
+
+            try
+            {
+                return BoxShadows.Parse(text);
+            }
+            catch (FormatException)
+            {
+                return default(BoxShadows);
+            }
+        }
+
+        public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            return value is BoxShadows shadows
+            ? shadows.ToString()
+            : BindingOperations.DoNothing;
+        }
+    }
+}
