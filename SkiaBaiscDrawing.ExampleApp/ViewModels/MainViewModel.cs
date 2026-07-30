d7e71a87872a9c39e7738e0fec1a56930564b459
Jasson_Chou <ss5014871@gmail.com>
2026-07-31 01:28:31 +0800

    嘗試修改成MVVM設計模式

--- SkiaBaiscDrawing.ExampleApp/ViewModels/MainViewModel.cs
+++ SkiaBaiscDrawing.ExampleApp/ViewModels/MainViewModel.cs
@@ -1,10 +1,24 @@
+using Avalonia.Collections;
 using CommunityToolkit.Mvvm.ComponentModel;
+using System.Collections.ObjectModel;
 
 namespace SkiaBasicDrawing.ExampleApp.ViewModels
 {
     public partial class MainViewModel : ViewModelBase
     {
         [ObservableProperty]
-        private string _greeting = "Welcome to Avalonia!";
+        private int _fps = 60;
+
+        [ObservableProperty]
+        private int _pointCount = 1000;
+
+        [ObservableProperty]
+        private float _minValue = -1.0f;
+
+        [ObservableProperty]
+        private float _maxValue = 1.0f;
+
+        [ObservableProperty]
+        private AvaloniaList<float> _values = new AvaloniaList<float>();
     }
 }
